2b9c2bf1ac86cf93fcc116cc52501ccfba9489ee
vlad0000 <52610344+vlad0000@users.noreply.github.com>
2020-08-21 12:40:16 +0300

    Animate

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-  
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.projects-pagination',
+    bulletClass: 'projects-bulet',
+    bulletActiveClass: 'projects-bulet-active',
+    clickable: true
+  },
 });
